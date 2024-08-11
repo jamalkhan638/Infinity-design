33d86edf9011ccaf8c97a7871c28934df2e05d6f
unknown <Jamal Ahmad>
2024-08-11 15:23:54 +0500

    done

--- src/components/ui/LogoBanner.jsx
+++ src/components/ui/LogoBanner.jsx
@@ -1,10 +1,16 @@
 import Image from "next/image";
+import { useRouter } from "next/navigation";
 import React from "react";
-
+import { usePathname } from "next/navigation";
 const LogoBanner = () => {
+  const router = useRouter()
+  const pathname = usePathname();
+  const hidebtn = pathname === "/login" || pathname === "/signup"
   return (
     <section className="position-relative bg-primary py-5">
+ { !hidebtn && <p onClick={()=>{router.push("/login"), localStorage.clear()}} className="logoutbtn">Logout</p> }
       <div className="text-cenrer d-flex justify-content-center">
+      
         <Image
           width={260}
           height={80}
